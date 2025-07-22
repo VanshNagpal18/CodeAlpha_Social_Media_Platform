bbc9650d9bf97673e866425dd792a78eb16842ce
Vansh Nagpal <vanshbharatnagpal@gmail.com>
2025-07-22 12:04:55 +0530

    script.js

--- script.js
+++ script.js
@@ -37,28 +37,28 @@ const messageSearch = document.querySelector('#message-search');
 
 
 
-// const searchMessage = () => {
-//     const val = messageSearch.value.toLowerCase();
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
    
-//     message.forEach(user => {
-//         let name = user.querySelector('h5').textContent.toLowerCase();
-//         if(name.indexOf(val) != -1){
-//             user.style.display = 'flex';
-//         } else{
-//             user.style.display = 'none';
-//         }
-//     })
-// }
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            user.style.display = 'flex';
+        } else{
+            user.style.display = 'none';
+        }
+    })
+}
 
-// messageSearch.addEventListener('keyup', searchMessage);
+messageSearch.addEventListener('keyup', searchMessage);
 
-// messagesNotification.addEventListener('click', () => {
-//      messages.style.boxShadow = '0 0 1rem var(--color-primary)';
-//      messagesNotification.querySelector('.notifications-count').style.display = 'none';
-//      setTimeout(() => {
-//        messages.style.boxShadow ='none';
-//      },2000);
-// })
+messagesNotification.addEventListener('click', () => {
+     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+     messagesNotification.querySelector('.notifications-count').style.display = 'none';
+     setTimeout(() => {
+       messages.style.boxShadow ='none';
+     },2000);
+})
 
 
 const openThemeModal = () => {
